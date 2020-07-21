5f648aa7908273886b3b96ec75dea9ccdfa147e7
David Piuva <Dawoodoz@hotmail.com>
2020-07-21 17:03:01 +0200

    Removing deprecated GUI header. Use the GUI API instead.

--- Source/DFPSR/gui/includeGui.h
+++
@@ -1,9 +0,0 @@
-
-// Header for including the most commonly needed parts of the framework
-
-#include "VisualTheme.h"
-#include "VisualComponent.h"
-#include "BackendWindow.h"
-#include "DsrWindow.h"
-#include "components/Panel.h"
-#include "components/Button.h"
