7c14e2bc67dcc69d23d95ca3dc10aeb6cea5f1b7
David Piuva <Dawoodoz@hotmail.com>
2020-09-04 16:06:45 +0200

    Corrected copyright year for the Label component.

--- Source/DFPSR/gui/components/Label.cpp
+++ Source/DFPSR/gui/components/Label.cpp
@@ -1,6 +1,6 @@
 // zlib open source license
 //
-// Copyright (c) 2018 to 2019 David Forsgren Piuva
+// Copyright (c) 2020 David Forsgren Piuva
 // 
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
