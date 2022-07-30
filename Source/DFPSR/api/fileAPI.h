b3b80ea8de8c8b50f9ce914c36ebbde6e28c6ea7
David Piuva <Dawoodoz@hotmail.com>
2022-07-30 11:49:50 +0200

    Made a smaller filesystem wrapper over the system APIs to be more reliable.

--- Source/DFPSR/api/fileAPI.h
+++ Source/DFPSR/api/fileAPI.h
@@ -21,24 +21,39 @@
 //    3. This notice may not be removed or altered from any source
 //    distribution.
 
+/*
+TODO:
+* bool file_setCurrentPath(const ReadableString& path);
+	WINBASEAPI WINBOOL WINAPI SetCurrentDirectoryW(LPCWSTR lpPathName);
+	chdir on Posix
+* bool file_createFolder(const ReadableString& path);
+	WINBASEAPI WINBOOL WINAPI CreateDirectoryW (LPCWSTR lpPathName, LPSECURITY_ATTRIBUTES lpSecurityAttributes);
+	mkdir on Posix
+* int64_t file_getSize(const ReadableString& path);
+	WINBASEAPI WINBOOL WINAPI GetFileSizeEx (HANDLE hFile, PLARGE_INTEGER lpFileSize);
+* bool file_remove(const ReadableString& path);
+	WINBASEAPI WINBOOL WINAPI DeleteFileW (LPCWSTR lpFileName);
+* bool file_exists(const ReadableString& path);
+	Can open a file without permissions and see if it works.
+* void file_getFolderContent(const ReadableString& folderPath, std::function<void(ReadableString, EntryType)> action)
+	How to do this the safest way with Unicode as a minimum requirement?
+*/
+
 #ifndef DFPSR_API_FILE
 #define DFPSR_API_FILE
 
 #include "../api/stringAPI.h"
 #include "bufferAPI.h"
-#include <version>
+#if defined(WIN32) || defined(_WIN32)
+	#define USE_MICROSOFT_WINDOWS
+#endif
+#if defined(__linux__)
+	#define USE_LINUX
+#endif
 
 // A module for file access that exists to prevent cyclic dependencies between strings and buffers.
 //   Buffers need a filename to be saved or loaded while strings use buffers to store their characters.
 namespace dsr {
-	// NativeChar is defined as whatever type the native system uses for expressing Unicode
-	// Use for input arguments in the main function to allow using file_convertInputArguments.
-	#if defined(WIN32) || defined(_WIN32)
-		using NativeChar = wchar_t; // UTF-16
-	#else
-		using NativeChar = char; // UTF-8
-	#endif
-
 	// Post-condition:
 	//   Returns the content of the file referred to be filename.
 	//   If mustExist is true, then failure to load will throw an exception.
@@ -53,44 +68,72 @@ namespace dsr {
 	//   Can be used to construct a file path that works for both forward and backward slash separators.
 	const char32_t* file_separator();
 
-	// Returns the local name after the last path separator, or the whole path if no separator was found.
+	// TODO: Create regression tests for the file system.
+
+	// Returns the local name of the file or folder after the last path separator, or the whole path if no separator was found.
+	// Examples with / as the path separator:
+	//   file_getFolderPath(U"MyFolder/Cars.txt") == U"Cars.txt"
+	//   file_getFolderPath(U"MyFolder/")         == U""
+	//   file_getFolderPath(U"MyFolder")          == U"MyFolder"
+	//   file_getFolderPath(U"MyFolder/Folder2")  == U"Folder2"
 	ReadableString file_getPathlessName(const ReadableString &path);
 
-	// Convert input arguments from main into a list of dsr::String.
-	// argv must be declared as wchar_t** on MS-Windows to allow using Unicode, which can be done by using the dsr::NativeChar alias:
-	//     int main(int argn, NativeChar **argv) {
-	List<String> file_convertInputArguments(int argn, NativeChar **argv);
+	// Returns the file's folder path with the file removed, or path unchanged if it was already a path.
+	// Does not include the last slash.
+	// Examples with / as the path separator:
+	//   file_getFolderPath(U"MyFolder/Documents/Cars.txt") == U"MyFolder/Documents"
+	//   file_getFolderPath(U"MyFolder/Documents/")         == U"MyFolder/Documents"
+	//   file_getFolderPath(U"MyFolder/Documents")          == U"MyFolder/Documents"
+	ReadableString file_getFolderPath(const ReadableString &path);
 
-	// Portable wrapper over the STD filesystem library, so that you get Unicode support with one type of string on all platforms.
-	// To enable this wrapper, just compile everything with C++17 or a newer version that still has the feature.
-	// Using more STD functionality can make it harder to port your project when C++ compilers are no longer maintained,
-	//   but exploring the filesystem is necessary for selecting files to load or save in editors.
-	#ifdef __cpp_lib_filesystem
-		enum class EntryType {
-			Unknown, Folder, File, SymbolicLink
-		};
-		// Get the current path, from where the application was called and relative paths start.
-		String file_getCurrentPath();
-		// Get the path's absolute form to know the exact location.
-		String file_getAbsolutePath(const ReadableString& path);
-		// Get the path's absolute form without redundancy.
-		String file_getCanonicalPath(const ReadableString& path);
-		// Gets a callback for each file and folder directly inside of folderPath.
-		// Calls back with the entry's path and an integer representing the entry type.
-		void file_getFolderContent(const ReadableString& folderPath, std::function<void(ReadableString, EntryType)> action);
-		// Returns true iff path refers to a valid file or folder.
-		bool file_exists(const ReadableString& path);
-		// Returns the file's size.
-		int64_t file_getSize(const ReadableString& path);
-		// Removes the file or empty folder.
-		// Returns true iff path existed.
-		bool file_remove(const ReadableString& path);
-		// Removes the file or folder including any content.
-		// Returns true iff path existed.
-		bool file_removeRecursively(const ReadableString& path);
-		// Returns true on success.
-		bool file_createFolder(const ReadableString& path);
+	// Combines two parts into a path and automatically adding a local separator when needed.
+	// Can be used to get the full path of a file in a folder or add another folder to the path.
+	// b may not begin with a separator, because only a is allowed to contain the root.
+	// Examples with / as the path separator:
+	//   file_combinePaths(U"Folder", U"Document.txt") == U"Folder/Document.txt"
+	//   file_combinePaths(U"Folder/", U"Document.txt") == U"Folder/Document.txt"
+	String file_combinePaths(const ReadableString &a, const ReadableString &b);
+
+	// Returns true iff path contains a root, according to the local path syntax.
+	bool file_hasRoot(const ReadableString &path);
+
+	// A convenient wrapper for getting input arguments as a list of portable Unicode strings.
+	//   The actual main function gets placed in DSR_MAIN_CALLER, which calls the given function.
+	// Example:
+	//   DSR_MAIN_CALLER(dsrMain)
+	//   int dsrMain(List<String> args) {
+	//       printText("Input arguments:\n");
+	//       for (int a = 0; a < args.length(); a++) {
+	//           printText("  args[", a, "] = ", args[a], "\n");
+	//       }
+	//       return 0;
+	//   }
+	#ifdef USE_MICROSOFT_WINDOWS
+		#define DSR_MAIN_CALLER(MAIN_NAME) \
+			int MAIN_NAME(List<String> args); \
+			int main() { \
+				return MAIN_NAME(file_impl_getInputArguments()); \
+			}
+	#else
+		#define DSR_MAIN_CALLER(MAIN_NAME) \
+			int MAIN_NAME(List<String> args); \
+			int main(int argc, char **argv) { return MAIN_NAME(file_impl_convertInputArguments(argc, (void**)argv)); }
 	#endif
+	// Helper functions have to be exposed for the macro handle your input arguments.
+	//   Do not call these yourself.
+	List<String> file_impl_convertInputArguments(int argn, void **argv);
+	List<String> file_impl_getInputArguments();
+
+	// Get the current path, from where the application was called and relative paths start.
+	String file_getCurrentPath();
+	// Get the application's folder path, from where the application is stored.
+	// If not implemented and allowFallback is true,
+	//   the current path is returned instead as a qualified guess instead of raising an exception.
+	String file_getApplicationFolder(bool allowFallback = true);
+	// Gets an absolute version of the path, quickly without removing redundancy.
+	String file_getAbsolutePath(const ReadableString &path);
+	// Returns true iff path refers to a valid file or folder.
+	bool file_exists(const ReadableString& path);
 }
 
 #endif
