0892c3ba0a3897d81a69b0628ae612070e4f8999
David Piuva <Dawoodoz@hotmail.com>
2022-08-05 18:16:54 +0200

    Using "end if" instead of "end" and "crawl" instead of "compile".

---
+++ Source/tools/builder/buildProject.sh
@@ -0,0 +1,46 @@
+# Using buildProject.sh
+#   $1 must be the *.DsrProj path, which is relative to the caller location.
+#   $2... are variable assignments sent as input to the given project file.
+#   BUILDER_CPP_COMPILER_PATH should be modified if it does not already refer to an installed C++ compiler.
+
+echo "Running buildProject.sh $@"
+
+# Get the build system's folder, where the build system is located
+BUILDER_FOLDER=`dirname "$(realpath $0)"`
+echo "BUILDER_FOLDER = ${BUILDER_FOLDER}"
+BUILDER_EXECUTABLE="${BUILDER_FOLDER}/builder"
+echo "BUILDER_EXECUTABLE = ${BUILDER_EXECUTABLE}"
+
+BUILDER_CPP_COMPILER_PATH="g++"
+echo "Change BUILDER_CPP_COMPILER_PATH in ${BUILDER_FOLDER}/buildProject.sh if you are not using ${BUILDER_CPP_COMPILER_PATH} as your compiler."
+
+# Check if the build system is compiled
+if [ -e "${BUILDER_EXECUTABLE}" ]; then
+	echo "Found the build system's binary."
+else
+	echo "Building the Builder build system for first time use."
+	LIBRARY_PATH="$(realpath ${BUILDER_FOLDER}/../../DFPSR)"
+	SOURCE_CODE="${BUILDER_FOLDER}/main.cpp ${BUILDER_FOLDER}/Machine.cpp ${BUILDER_FOLDER}/generator.cpp ${LIBRARY_PATH}/collection/collections.cpp ${LIBRARY_PATH}/api/fileAPI.cpp ${LIBRARY_PATH}/api/bufferAPI.cpp ${LIBRARY_PATH}/api/stringAPI.cpp ${LIBRARY_PATH}/base/SafePointer.cpp"
+	"${BUILDER_CPP_COMPILER_PATH}" -o "${BUILDER_EXECUTABLE}" ${SOURCE_CODE} -std=c++14
+	if [ $? -ne 0 ]
+	then
+		echo "Failed building the Builder build system, which is needed to build your project!"
+		exit 1
+	fi
+	chmod +x "${BUILDER_EXECUTABLE}"
+fi
+
+# Call the build system with a filename for the output script, which is later given execution permission and called.
+SCRIPT_PATH="/tmp/dfpsr_compile.sh"
+echo "Generating ${SCRIPT_PATH} from $1"
+rm "${SCRIPT_PATH}"
+"${BUILDER_EXECUTABLE}" $@ "ScriptPath=${SCRIPT_PATH}";
+if [ $? -ne 0 ]
+then
+	echo "Failed building the Builder build system, which is needed to build your project!"
+	exit 1
+fi
+echo "Giving execution permission to ${SCRIPT_PATH}"
+chmod +x "${SCRIPT_PATH}"
+echo "Running ${SCRIPT_PATH}"
+"${SCRIPT_PATH}"
