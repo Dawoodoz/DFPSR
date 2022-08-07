3e3143d62d4225c5a79f7f4e5fd5928bb4ebcdd0
David Piuva <Dawoodoz@hotmail.com>
2022-08-07 17:39:33 +0200

    Removed old build files from templates, because the newer system is easier to explain. Made an SDK explorer as a first feature of the Wizard application, but launching anything is still not implemented and it needs a custom GUI theme.

--- Source/templates/Readme.txt
+++ Source/templates/Readme.txt
@@ -1,8 +1,28 @@
-To get started with your own project:
+﻿To get started with your own project:
 * Copy one of these folders into a location with stable relative access to the DFPSR library.
-  You can also place DFPSR in a standard path and refer to it using absolute paths.
+	You can also place DFPSR in a standard path, refer to it using absolute paths and always have your code in the same location.
+	On Linux, you can also place a symbolic link right next to your projects and use relative paths using the symbolic link as a folder name.
 * Change #include "../../DFPSR/includeFramework.h" in main.cpp to find the header from the new location.
-* Change ROOT_PATH=../.. the same way in build.sh or just delete the whole build script if you're not going to use it.
-* Give execution permission using chmod +x build.sh. (if using Linux)
-* Build and run your new application. (./build.sh if using Linux)
+* Change path for tools/builder/buildProject.sh in build_linux.sh, and tools/builder/buildProject.bat in build_windows.bat,
+	so that they give your project file to the build system.
+* Change path for DFPSR/DFPSR.DsrHead in main.DsrProj if imported in your project.
 
+Build on Linux:
+* Give execution permission using:
+	chmod +x build_linux.sh
+* Build using:
+	./build_linux.sh
+"./" for the relative path is needed to distinguish the path from global aliases for the first argument, but not needed when given as input to a program.
+
+Build on Windows:
+* Install the MinGW edition of CodeBlocks.
+	Both to get an IDE that can be used for debugging, and because it's the easiest way to install a GNU compiler on MS-Windows.
+* Update CPP_COMPILER_PATH and CPP_COMPILER_FOLDER to reference where your C++ compiler is located.
+	If you installed CodeBlocks in C:\Program, it might work without any changes.
+	CPP_COMPILER_PATH is the absolute path to the specific compiler's binary.
+	CPP_COMPILER_FOLDER is the folder to launch the compiler from.
+	Any dynamic dependency being used by the compiler that is not registered (regsvr32 if it's self registering),
+	will be loaded from the current directory and not the compiler's own folder (since Windows XP).
+	Therefore the build system will temporarily enter the folder specified by CPP_COMPILER_FOLDER as the CompileFrom flag,
+	so that the compiler's dependencies can be loaded from the compiler's own folder where DLL files are usually stored.
+* Double click on buildProject.bat to build and run.
--- Source/tools/wizard/main.cpp
+++ Source/tools/wizard/main.cpp
@@ -5,6 +5,8 @@
 //     How can the file library execute other applications and scripts in a portable way when scripts need to select a terminal application to execute them?
 //     Maybe call the builder as a static library and have it call the compiler directly in a simulated terminal window embedded into the wizard, instead of using unreliable scripts?
 // * Let the user browse a file system and select a location for a new or existing project.
+//     Should a multi-frame tab container be created to allow having multiple frames in the same container?
+//         Can let frames have a caption for when used within a container.
 
 #include "../../DFPSR/includeFramework.h"
 #include "sound.h"
@@ -24,16 +26,16 @@ Begin : Panel
 		Name = "upperPanel"
 		Bottom = 50
 		Solid = 1
-		Color = 120,120,100
+		Color = 190,255,190
 	End
 	Begin : Panel
 		Name = "lowerPanel"
 		Solid = 1
 		Top = 50
-		Color = 120,120,100
+		Color = 0,0,0
 		Begin : ListBox
 			Name = "projectList"
-			Color = 180,180,140
+			Color = 190,255,190
 			Left = 90%-100
 			Right = 100%-5
 			Top = 5
@@ -42,18 +44,26 @@ Begin : Panel
 		Begin : Button
 			Name = "buildButton"
 			Text = "Build and run"
-			Color = 160,150,150
+			Color = 190,255,190
 			Left = 90%-100
 			Right = 100%-5
 			Top = 100%-45
 			Bottom = 100%-5
 		End
+		Begin : Picture
+			Name = "previewPicture"
+			Interpolation = 1
+			Left = 5
+			Top = 5
+			Right = 90%-105
+			Bottom = 70%-5
+		End
 		Begin : Label
 			Name = "descriptionLabel"
-			Text = "DFPSR wizard application"
+			Color = 190,255,190
 			Left = 5
 			Right = 90%-105
-			Top = 5
+			Top = 70%
 			Bottom = 100%-5
 		End
 	End
@@ -64,17 +74,42 @@ End
 Component projectList;
 Component buildButton;
 Component descriptionLabel;
+Component previewPicture;
 
 // Media
 int boomSound;
 
 struct Project {
 	String buildScript; // To execute
-	String projectFolderName; // To display
-	Project(const ReadableString &buildScript, const ReadableString &projectFolderName)
-	: buildScript(buildScript), projectFolderName(projectFolderName) {}
+	String title; // To display
+	String description; // To show when selected
+	OrderedImageRgbaU8 preview;
+	Project(const ReadableString &buildScript);
 };
 List<Project> projects;
+
+Project::Project(const ReadableString &buildScript)
+: buildScript(buildScript) {
+	String projectFolderPath = file_getRelativeParentFolder(buildScript);
+	this->title = file_getPathlessName(projectFolderPath);
+	String descriptionPath = file_combinePaths(projectFolderPath, U"Description.txt");
+	if (file_getEntryType(descriptionPath) == EntryType::File) {
+		this->description = string_load(descriptionPath);
+	} else {
+		this->description = string_combine(U"Project at ", projectFolderPath, U" did not have any Description.txt to display!");
+	}
+	String previewPath = file_combinePaths(projectFolderPath, U"Preview.jpg");
+	if (file_getEntryType(previewPath) == EntryType::File) {
+		this->preview = image_load_RgbaU8(previewPath);
+	} else {
+		previewPath = file_combinePaths(projectFolderPath, U"Preview.gif");
+		if (file_getEntryType(previewPath) == EntryType::File) {
+			this->preview = image_load_RgbaU8(previewPath);
+		} else {
+			this->preview = OrderedImageRgbaU8();
+		}
+	}
+}
 
 static ReadableString findParent(const ReadableString& startPath, const ReadableString& parentName) {
 	int64_t pathEndIndex = -1; // Last character of path leading to Source.
@@ -97,25 +132,38 @@ static void findProjects(const ReadableString& folderPath) {
 			findProjects(entryPath);
 		} else if (entryType == EntryType::File) {
 			ReadableString extension = string_upperCase(file_getExtension(entryName));
+			Project newProject = Project(entryPath);
 			// If we find a project within folderPath...
 			if (string_match(extension, U"DSRPROJ")) {
-				String projectFolderName = file_getPathlessName(file_getRelativeParentFolder(entryPath));
 				// ...and the folder is not namned wizard...
-				if (!string_match(projectFolderName, U"wizard")) {
+				if (!string_match(newProject.title, U"wizard")) {
 					// ...then add it to the list of projects.
-					projects.pushConstruct(entryPath, projectFolderName);
+					projects.push(newProject);
 				}
 			}
 		}
 	});
 }
 
+static void selectProject(int64_t index) {
+	int oldIndex = component_getProperty_integer(projectList, U"SelectedIndex", true);
+	printText(oldIndex, U" -> ", index,  U"\n");
+	// Don't trigger new events if the selected index is already updated manually.
+	if (index != oldIndex) {
+		printText(U"Assigned ", index,  U"\n");
+		component_setProperty_integer(projectList, U"SelectedIndex", index, false);
+	}
+	printText(U"Assigning description\n");
+	component_setProperty_string(descriptionLabel, U"Text", projects[index].description);
+	component_setProperty_image(previewPicture, U"Image", projects[index].preview, false);
+}
+
 static void populateInterface(const ReadableString& folderPath) {
 	findProjects(folderPath);
 	for (int p = 0; p < projects.length(); p++) {
-		component_call(projectList, U"PushElement", projects[p].projectFolderName);
+		component_call(projectList, U"PushElement", projects[p].title);
 	}
-	component_setProperty_integer(projectList, U"SelectedIndex", 0, false);
+	selectProject(0);
 }
 
 DSR_MAIN_CALLER(dsrMain)
@@ -132,6 +180,7 @@ void dsrMain(List<String> args) {
 	projectList = window_findComponentByName(window, U"projectList");
 	buildButton = window_findComponentByName(window, U"buildButton");
 	descriptionLabel = window_findComponentByName(window, U"descriptionLabel");
+	previewPicture = window_findComponentByName(window, U"previewPicture");
 
 	// Find projects to showcase
 	//   On systems that don't allow getting the application's folder, the program must be started somewhere within the Source folder.
@@ -150,12 +199,13 @@ void dsrMain(List<String> args) {
 	});
 	component_setPressedEvent(buildButton, []() {
 		// TODO: Implement building and running of the selected project.
+		playSound(boomSound, false, 1.0, 1.0, 0.7);
 		component_setProperty_string(descriptionLabel, U"Text", U"Compiling and running projects from the wizard application is not yet implemented.");
 	});
 	component_setSelectEvent(projectList, [](int64_t index) {
-		// TODO: Load description from a text file with a specific name in the project's folder.
+		printText(U"Selecting ", index, U"\n");
 		playSound(boomSound, false, 0.5, 0.5, 0.5);
-		component_setProperty_string(descriptionLabel, U"Text", string_combine(U"Project at ", projects[index].buildScript));
+		selectProject(index);
 	});
 	window_setCloseEvent(window, []() {
 		running = false;
