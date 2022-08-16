7b1d289a62aa6d7970999a92f2a0dc4ca3d4403c
David Piuva <Dawoodoz@hotmail.com>
2022-08-16 20:53:55 +0200

    These incorrect path separators were somehow accepted on Windows 7.

--- Source/templates/basic3D/build_windows.bat
+++ Source/templates/basic3D/build_windows.bat
@@ -3,4 +3,4 @@
 rem Launch the build system with Basic3D.DsrProj and Windows selected as the platform.
 
 echo "Running build_windows.bat %@%
-../../tools/builder/buildProject.bat Basic3D.DsrProj Windows %@%
+..\..\tools\builder\buildProject.bat Basic3D.DsrProj Windows %@%
--- Source/templates/basicGUI/build_windows.bat
+++ Source/templates/basicGUI/build_windows.bat
@@ -3,4 +3,4 @@
 rem Launch the build system with BasicGUI.DsrProj and Windows selected as the platform.
 
 echo "Running build_windows.bat %@%
-../../tools/builder/buildProject.bat BasicGUI.DsrProj Windows %@%
+..\..\tools\builder\buildProject.bat BasicGUI.DsrProj Windows %@%
