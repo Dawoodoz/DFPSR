02e10466ef6b044bae570203c9c6b8028e1c1ea2
David Piuva <davidpiuva@Mac-mini-som-tillhor-David.local>
2025-04-07 16:22:28 +0200

    Made an empty Objective-C++ file to test linking with Cocoa.

---
+++ Source/windowManagers/CocoaWindow.mm
@@ -0,0 +1,11 @@
+﻿
+#import <Cocoa/Cocoa.h>
+
+#include "../DFPSR/base/Handle.h"
+#include "../DFPSR/implementation/gui/BackendWindow.h"
+#include "../DFPSR/api/stringAPI.h"
+
+dsr::Handle<dsr::BackendWindow> createBackendWindow(const dsr::String& title, int width, int height) {
+	dsr::sendWarning("Tried to create a DsrWindow with Cocoa!\n");
+	return dsr::Handle<dsr::BackendWindow>();
+}
