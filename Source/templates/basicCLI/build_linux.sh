671cd45c4eecdddd8c1773f73fcb58b5cde07b28
David Piuva <dawoodoz@hotmail.com>
2025-05-06 20:53:55 +0200

    Simplified build wrappers for template projects.

--- Source/templates/basicCLI/build_linux.sh
+++ Source/templates/basicCLI/build_linux.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-
-# Launch the build system with BasicCLI.DsrProj and Linux selected as the platform.
-echo "Running build_linux.sh $@"
 chmod +x ../../tools/builder/buildProject.sh;
 ../../tools/builder/buildProject.sh BasicCLI.DsrProj Linux $@;
--- Source/templates/basicGUI/build_linux.sh
+++ Source/templates/basicGUI/build_linux.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-
-# Launch the build system with BasicGUI.DsrProj and Linux selected as the platform.
-echo "Running build_linux.sh $@"
 chmod +x ../../tools/builder/buildProject.sh;
 ../../tools/builder/buildProject.sh BasicGUI.DsrProj Linux $@;
