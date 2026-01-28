aa880750de73458e21121f5684c45ecd06be114d
David Piuva <david.piuva@dawoodoz.com>
2026-01-28 20:09:22 +0100

    Created FixedArray with a size decided in compile-time to avoid dynamic allocation but still get bound checks.

--- Source/DFPSR/collection/includeCollection.h
+++ Source/DFPSR/collection/includeCollection.h
@@ -1,6 +1,10 @@
 ﻿
 // Header for including collections
 
+// Using recycled memory from base/heap.cpp
 #include "Array.h"
 #include "Field.h"
 #include "List.h"
+
+// Allocating directly where the collection is placed
+#include "FixedArray.h"
